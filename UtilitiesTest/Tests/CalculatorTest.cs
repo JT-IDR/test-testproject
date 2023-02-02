37e93afcb13078114ba2ae737983cf7572bef796
JT-IDR <117637963+JT-IDR@users.noreply.github.com>
2023-02-02 14:51:56 +0200

    Initial commit

---
+++ UtilitiesTest/Tests/CalculatorTest.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using UtilitiesCore.Interfaces;
+
+namespace InterviewTestsCore
+{
+    [TestFixture]
+    public class CalculatorTest
+    {
+        [TestCase("1 + 1", 2)]
+        [TestCase("201 - 201", 0)]
+        [TestCase("50 - 45 + 45", 50)]
+        [TestCase("1 + 5 + 100 - 26", 80)]
+        [TestCase("1 + 1 + 1 - 1 - 1 + 1", 2)]
+        public void Calculator(string expression, int expectedResult)
+        {
+            // Arrange
+            ICalculator calculator = null;
+            
+            // Act
+            var result = calculator.Calculate(expression);
+            
+            // Assert
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+    }
+}
---
+++ UtilitiesTest/Tests/NumberCounterTest.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using UtilitiesCore.Interfaces;
+
+namespace InterviewTests
+{
+    [TestFixture]
+    public class NumberCounterTest
+    {
+        [TestCase("1", 1)]
+        [TestCase("1x", 1)]
+        [TestCase("999x", 3)]
+        [TestCase("abc1234x", 4)]
+        [TestCase(" / * 4 ttt 8|", 2)]
+        public void CountNumbers(string input, int expectedCount)
+        {
+            // Arrange
+            INumberCounter numberCounter = null;
+            
+            // Act
+            var result = numberCounter.CountNumbers(input);
+            
+            // Assert
+            Assert.That(result, Is.EqualTo(expectedCount));
+        }
+    }
+}
